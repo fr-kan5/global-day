ad52acfc8eee395cd48ed882b66573157a6bc319
liz539z <abc11nu@gmail.com>
2020-10-23 20:13:02 +0900

    ログイン後の遷移ページを一覧画面（post/index)へ変更した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :country_id, :birthday, :profile])
   end
+
+  def after_sign_in_path_for(resource)
+    posts_path(resource)
+  end
 end
