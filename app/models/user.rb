821a571ab38557eca4aa610464b96fc7142e48ac
liz539z <abc11nu@gmail.com>
2020-10-23 19:38:23 +0900

    Userモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :country
+
+  with_options presence: true do
+    validates :name
+    validates :password_confirmation
+    validates :country_id, numericality: { other_than: 1 }
+    validates :birthday
+  end
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'Password Include both letters and numbers' }
 end
