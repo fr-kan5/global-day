f9cd4cfbae5308433376d963131fb86921cb9a7b
liz539z <abc11nu@gmail.com>
2020-10-26 18:14:24 +0900

    postsコントローラー編集　privateメソッド作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,21 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path(current_user.id)
+    else
+      render action: :new
+    end
+
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text, :place, :image).merge(user_id: current_user.id)
   end
 end
