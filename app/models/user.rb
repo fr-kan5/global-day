2ac7d87ea81ab7c43376f8ef0c1dbae9dc196cf2
liz539z <abc11nu@gmail.com>
2020-10-26 18:13:39 +0900

    Userモデル編集　（アソシエーション追記）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :posts
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :country
 
