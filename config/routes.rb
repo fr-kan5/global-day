6239172ad037ecf44f6dae4fe7e04232d574b7cb
liz539z <abc11nu@gmail.com>
2020-10-28 20:15:59 +0900

    ルーティング設置

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#index'
   resources :homes, only: :index
-  resources :posts
+  resources :posts do
+    resources :comments, only: [:index,:create]
+  end
 end
