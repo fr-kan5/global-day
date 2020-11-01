c197ae21dd6a4bcc9ff6dc0c98c52aea2524c26d
liz539z <abc11nu@gmail.com>
2020-11-01 13:39:23 +0900

    コントローラーの設置

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def show
     @user = User.find(params[:id])
     @posts = current_user.posts
+  end
+
+  def edit
+  end
+
+  def update
   end
 end
