848d5b107d319a30f138709d1126b1c92a6b8771
liz539z <abc11nu@gmail.com>
2020-11-02 16:02:04 +0900

    ルーティング編集（ユーザー編集のためdevise部分の変更）

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users , controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   root to: 'homes#index'
   resources :homes, only: :index
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: :show
   resources :posts do
     resources :comments, only: :create
   end
