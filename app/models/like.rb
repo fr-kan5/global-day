ec51691867414ff14707acfe23e4d82cf3f2dbef
liz539z <abc11nu@gmail.com>
2020-11-03 17:51:36 +0900

    Likeモデルの追加・修正

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,5 @@
 class Like < ApplicationRecord
-  validates :user_id, presence: true, uniqueness: {scope: :post_id}
-  validates :post_id, presence: true
   belongs_to :user
   belongs_to :post
+  validates_uniqueness_of :post_id, scope: :user_id
 end
