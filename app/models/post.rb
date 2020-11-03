25c65c8e0d12a5f611384e721fb3f95151d63ac4
liz539z <abc11nu@gmail.com>
2020-11-03 17:51:07 +0900

    Postモデルの追加・修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :likes, dependent: :destroy
+  has_many :likes
+  has_many :users, through: :likes
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
   has_many :comments, dependent: :destroy
   has_one_attached :image
   with_options presence: true do
