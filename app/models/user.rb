6479766e6ab719b34c9eb66c718353f7391dcda6
liz539z <abc11nu@gmail.com>
2020-11-03 17:50:48 +0900

    Userモデルの追加・修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
   has_many :likes, dependent: :destroy
+  has_many :like_posts, through: :likes, source: :post
   has_many :comments, dependent: :destroy
   has_one_attached :avatar  
   extend ActiveHash::Associations::ActiveRecordExtensions
