69743b931a6d761695e5422a8e6957ba1bd8ba12
liz539z <abc11nu@gmail.com>
2020-11-05 21:49:42 +0900

    マイグレーションファイル作成

--- db/migrate/20201105124522_create_memos.rb
+++ db/migrate/20201105124522_create_memos.rb
@@ -2,6 +2,9 @@ class CreateMemos < ActiveRecord::Migration[6.0]
   def change
     create_table :memos do |t|
 
+      t.string :title, null: false
+      t.text :content, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_02_102957) do
+ActiveRecord::Schema.define(version: 2020_11_05_124522) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -50,6 +50,15 @@ ActiveRecord::Schema.define(version: 2020_11_02_102957) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
+  create_table "memos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_memos_on_user_id"
+  end
+
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "text", null: false
@@ -79,5 +88,6 @@ ActiveRecord::Schema.define(version: 2020_11_02_102957) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "memos", "users"
   add_foreign_key "posts", "users"
 end
