b40f8af386fe23094afa81f6d615378a190136dc
liz539z <abc11nu@gmail.com>
2020-11-06 13:32:33 +0900

    memosコントローラー 作成

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,2 +1,20 @@
 class MemosController < ApplicationController
+  before_action :authenticate_user!
+
+  def index 
+    @memos = Memo.includes(:user)
+    @memo = Memo.new
+  end
+
+  def create
+    @memo = Memo.new(params_memo)
+    @memo.save
+    redirect_to user_memos_path(current_user)
+
+  end
+
+  private
+  def params_memo
+    params.require(:memo).permit(:title, :content).merge(user_id: current_user.id)
+  end
 end
