268b41317b12552ad12f13384294fbcb214d6048
liz539z <abc11nu@gmail.com>
2020-11-18 22:22:17 +0900

    validation 変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   with_options presence: true do
-    validates :title
-    validates :text
-    validates :place
+    ENG_REGEX = /\A[a-zA-Z]+\z/.freeze
+    with_options length: {maximum: 30} do
+    validates :title, format: {with: ENG_REGEX, message: 'is only ENGLISH'}
+    validates :place, format: {with: ENG_REGEX, message: 'is only ENGLISH'}
+    end
+    validates :text, format: {with: ENG_REGEX, message: 'is only ENGLISH'}
     validates :image
   end
 end
